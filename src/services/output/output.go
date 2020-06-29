08eb5100de288c07cb0aa6e5e8b62cdd856bb31c
DH <adlandh@gmail.com>
2020-06-29 20:58:42 +0200

    parallel conversion of the albums

--- src/services/output/output.go
+++ src/services/output/output.go
@@ -3,45 +3,35 @@ package output
 import (
 	"fmt"
 	"io"
+	"log"
 	"os"
+	"sync"
 
 	"github.com/adlandh/flickr-data-converter/src/models"
 )
 
 type Output struct {
-	settings *models.Settings
+	Settings *models.Settings
 	Albums   models.Albums
 	Photos   models.Photos
+	albumsWg *sync.WaitGroup
 }
 
-func (o Output) Generatefolders() error {
+func (o *Output) Generatefolders() {
 	for _, album := range o.Albums {
-		albumDir := o.settings.Output + string(os.PathSeparator) + album.Title
-		err := os.Mkdir(albumDir, 0755)
-		if err != nil && !os.IsExist(err) {
-			return models.NewOutputError(albumDir, "", err)
-		}
-		for _, photoId := range album.Photos {
-			if photoId == "0" {
-				continue
-			}
-			srcFile := o.Photos[photoId].FileName
-			dstFile := albumDir + string(os.PathSeparator) + o.Photos[photoId].Name + ".jpg"
-			_, err := o.copy(srcFile, dstFile)
-			if err != nil {
-				return models.NewOutputError(albumDir, photoId, err)
-			}
-		}
+		o.albumsWg.Add(1)
+		go o.generateAlbumFolder(album)
 	}
 
-	return nil
+	o.albumsWg.Wait()
 }
 
 func New(settings *models.Settings, albums models.Albums, photos models.Photos) Output {
 	return Output{
-		settings: settings,
+		Settings: settings,
 		Albums:   albums,
 		Photos:   photos,
+		albumsWg: &sync.WaitGroup{},
 	}
 }
 
@@ -68,4 +58,27 @@ func (o Output) copy(src, dst string) (int64, error) {
 	defer destination.Close()
 	nBytes, err := io.Copy(destination, source)
 	return nBytes, err
+}
+
+func (o *Output) generateAlbumFolder(album models.Album) {
+	defer o.albumsWg.Done()
+
+	albumDir := o.Settings.Output + string(os.PathSeparator) + album.Title
+	err := os.Mkdir(albumDir, 0755)
+	if err != nil && !os.IsExist(err) {
+		log.Fatal(models.NewOutputError(albumDir, "", err))
+	}
+
+	for _, photoId := range album.Photos {
+		if photoId == "0" {
+			continue
+		}
+
+		srcFile := o.Photos[photoId].FileName
+		dstFile := albumDir + string(os.PathSeparator) + o.Photos[photoId].Name + ".jpg"
+		_, err := o.copy(srcFile, dstFile)
+		if err != nil {
+			log.Fatal(models.NewOutputError(albumDir, photoId, err))
+		}
+	}
 }
